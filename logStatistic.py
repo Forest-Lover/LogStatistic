438f7f8ec758c5925ffc95a866f23f68e05e06df
Forest-Lover <1771880185@qq.com>
2024-04-03 15:04:08 +0800

    first commit

---
+++ logStatistic.py
@@ -0,0 +1,364 @@
+# -*- coding: utf-8 -*-
+import argparse
+import json
+import glob
+import re
+import psutil
+import os
+import threading
+import time
+import numpy as np
+from decimal import Decimal, ROUND_HALF_UP
+from collections import Counter, defaultdict
+from datetime import datetime, timedelta
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import DBSCAN, KMeans
+from sklearn.metrics.pairwise import cosine_similarity
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor, as_completed
+
+def memory_watcher(max_memory, check_interval):
+    """
+    Monitor the memory usage of the current process in a separate thread.
+    :param max_memory: Maximum memory in MB.
+    :param check_interval: Time interval in seconds between memory checks.
+    """
+    process = psutil.Process()
+    start_time = time.time()
+    while True:
+        current_time = time.time()
+        elapsed_time = current_time - start_time
+        memory_usage = process.memory_info().rss / (1024 * 1024)
+        if memory_usage > max_memory:
+            print(f"Memory limit exceeded: {memory_usage:.2f} MB used; limit is {max_memory} MB.")
+            os._exit(1)
+        if elapsed_time > 30:
+            # print(f"Run {elapsed_time: .0f}s, Current memory usage: {memory_usage:.2f} MB.")
+            pass
+        time.sleep(check_interval)
+
+def start_memory_watcher(max_memory, check_interval=1):
+    """
+    Start the memory watcher in a daemon thread.
+    :param max_memory: Maximum memory in MB.
+    :param check_interval: Time interval in seconds between memory checks.
+    """
+    watcher_thread = threading.Thread(target=memory_watcher, args=(max_memory, check_interval), daemon=True)
+    watcher_thread.start()
+
+def extract_bracket_contents(log_line, left='[', right=']'):
+    parts = []
+    stack = []
+    temp = ""
+    for char in log_line:
+        if char == left:
+            if stack:
+                temp += char
+            stack.append(char)
+        elif char == right:
+            if len(stack) == 1:
+                parts.append(temp)
+                temp = ""
+            else:
+                temp += char
+            stack.pop()
+        else:
+            if stack:
+                temp += char
+    # 如果栈不为空，说明有未闭合的括号
+    if stack:
+        raise ValueError("Unmatched brackets in the string")
+    return parts
+
+def parse_timestamp(log):
+    # 2024-04-01 20:54:53.415 或 2024-04-02T20:46:23.415+0800
+    for fmt in ('%Y-%m-%d %H:%M:%S.%f', '%Y-%m-%dT%H:%M:%S.%f%z'):
+        try:
+            return datetime.strptime(log, fmt)
+        except ValueError:
+            continue
+    # 如果都不匹配，返回一个特定的默认时间戳
+    return datetime(1970, 1, 1)
+
+def parse_significant_digits(log, significant_digits):
+    # 四舍五入到指定有效位
+    pattern = re.compile(r'([-+]?\d*\.\d+|[-+]?\d+)')
+    match = pattern.search(log)
+    if match:
+        number_str = match.group(0)
+        number_decimal = Decimal(number_str)
+        # 移动小数点使得只有有效位数的数字在小数点前
+        shift = number_decimal.adjusted() - significant_digits + 1
+        # 四舍五入到有效位数
+        rounded_number = number_decimal.scaleb(-shift).quantize(Decimal('1'), rounding=ROUND_HALF_UP).scaleb(shift)
+        start, end = match.span(0)
+        log = log[:start] + str(rounded_number) + log[end:]
+    return log
+
+def parse_decimal_places(log, decimal_places):
+    # 四舍五入到指定小数位
+    pattern = re.compile(r'([-+]?\d*\.\d+|[-+]?\d+)')
+    match = pattern.search(log)
+    if match:
+        number_str = match.group(0)
+        rounded_number = Decimal(number_str).quantize(Decimal('1.' + '0' * decimal_places), rounding=ROUND_HALF_UP)
+        start, end = match.span(0)
+        log = log[:start] + str(rounded_number) + log[end:]
+    return log
+
+# 预处理函数
+def preprocess_log(log_line, line_filter_def):
+    log_parts = {}
+    if line_filter_def['seperator'] == 'brackets':
+        if not log_line.startswith('[') or not log_line.strip().endswith(']'):
+            parts = []
+        else:
+            parts = extract_bracket_contents(log_line)
+            # parts = re.findall(r'\[(?:[^\[\]]|\[[^\[\]]*\])*\]', log_line)
+            # parts = re.findall(r'\[([^[\]]+(?:\[[^\]]*\])?[^[\]]*)\]', log_line)
+        for key, index in line_filter_def['mapping'].items():
+            try:
+                log_parts[key] = parts[index - 1]
+            except IndexError:
+                log_parts[key] = "null"
+    elif line_filter_def['seperator'] == 'json':
+        try:
+            log_json = json.loads(log_line)
+            for key, field in line_filter_def['mapping'].items():
+                log_parts[key] = log_json.get(field, "null")
+        except json.JSONDecodeError:
+            for key in line_filter_def['mapping'].keys():
+                log_parts[key] = "null"
+    else:
+        parts = log_line.strip().split(line_filter_def['seperator'])
+        for key, index in line_filter_def['mapping'].items():
+            try:
+                log_parts[key] = parts[index - 1]
+            except IndexError:
+                log_parts[key] = "null"
+    return log_parts
+
+# 聚类函数
+def group_log(logs, merge_def):
+    groups = []
+    group_info = defaultdict(list)
+    group_indexes = defaultdict(list)
+    if not logs:
+        return groups, group_info, group_indexes
+    if merge_def == 'DBSCAN':
+        vectorizer = TfidfVectorizer()
+        X = vectorizer.fit_transform(logs)
+        dbscan = DBSCAN(eps=0.5, min_samples=5)
+        dbscan.fit(X)
+        groups = dbscan.labels_
+        # 为DBSCAN生成簇信息和索引
+        for idx, label in enumerate(groups):
+            group_indexes[label].append(idx)
+            if label != -1:
+                group_info[label].append(logs[idx])
+        for label, log_list in group_info.items():
+            if label != -1:
+                group_info[label] = log_list[:1]  # top1
+    elif merge_def.startswith('K_MEANS'):
+        num_groups = int(merge_def.split('-')[1])
+        vectorizer = TfidfVectorizer()
+        X = vectorizer.fit_transform(logs)
+        kmeans = KMeans(n_clusters=num_groups, random_state=42)
+        kmeans.fit(X)
+        groups = kmeans.labels_
+        # 为K_MEANS生成簇信息和索引
+        order_centroids = kmeans.cluster_centers_.argsort()[:, ::-1]
+        terms = vectorizer.get_feature_names_out()
+        for i in range(num_groups):
+            group_indexes[i] = list(np.where(groups == i)[0])
+            centroid_vector = kmeans.cluster_centers_[i]
+            log_vectors = X[groups == i]
+            if log_vectors.shape[0] > 0:
+                similarities = cosine_similarity(centroid_vector.reshape(1, -1), log_vectors)
+                top_index = similarities.argsort()[0][-1:]  # top1
+                for index in top_index:
+                    original_log_index = group_indexes[i][index]
+                    group_info[i].append(logs[original_log_index])
+    elif merge_def.startswith('TIME'):
+        time_interval = int(merge_def.split('-')[1])
+        timestamps = [parse_timestamp(log) for log in logs]
+        if timestamps:
+            min_time = min(timestamps)
+            min_time_rounded = min_time.replace(minute=0, second=0, microsecond=0)
+            groups = [int((timestamp - min_time_rounded).total_seconds() // time_interval) for timestamp in timestamps]
+            # 为TIME生成簇信息和索引
+            for idx, group_id in enumerate(groups):
+                group_indexes[group_id].append(idx)
+                start_time = min_time_rounded + timedelta(seconds=group_id * time_interval)
+                end_time = start_time + timedelta(seconds=time_interval)
+                group_info[group_id] = {
+                    'start': start_time.strftime('%Y-%m-%d %H:%M:%S.%f'),
+                    'end': end_time.strftime('%Y-%m-%d %H:%M:%S.%f')
+                }
+    elif merge_def.startswith('ROUND_DECIMAL'):
+        decimal_places = int(merge_def.split('-')[1])
+        values = [parse_decimal_places(log, decimal_places) for log in logs]
+        if values:
+            unique_logs = list(set(values))
+            groups = [unique_logs.index(log) for log in values]
+            # 为ROUND_DECIMAL生成簇信息和索引
+            for idx, log in enumerate(values):
+                group_id = unique_logs.index(log)
+                group_indexes[group_id].append(idx)
+            group_info = {i: log for i, log in enumerate(unique_logs)}
+    elif merge_def.startswith('ROUND_SIGNIFICANT'):
+        decimal_places = int(merge_def.split('-')[1])
+        values = [parse_significant_digits(log, decimal_places) for log in logs]
+        if values:
+            unique_logs = list(set(values))
+            groups = [unique_logs.index(log) for log in values]
+            # 为ROUND_SIGNIFICANT生成簇信息和索引
+            for idx, log in enumerate(values):
+                group_id = unique_logs.index(log)
+                group_indexes[group_id].append(idx)
+            group_info = {i: log for i, log in enumerate(unique_logs)}
+    elif merge_def == 'EQUAL':
+        unique_logs = list(set(logs))
+        groups = [unique_logs.index(log) for log in logs]
+        # 为EQUAL生成簇信息和索引
+        for idx, log in enumerate(logs):
+            group_id = unique_logs.index(log)
+            group_indexes[group_id].append(idx)
+        group_info = {i: log for i, log in enumerate(unique_logs)}
+    else:
+        raise ValueError(f"Unsupported merge definition: {merge_def}")
+    # 返回聚类结果和每个簇的信息和索引
+    return groups, group_info, group_indexes
+
+# 主处理函数
+def process_log(input_config, output_config, define):
+    if output_config['merge_file']:
+        preprocessed_logs = []
+        for file_key in input_config['file_filter'].keys():
+            process_input(preprocessed_logs, input_config, define, file_key)
+        process_output(preprocessed_logs, output_config, define, "MERGED")
+    else:
+        for file_key in input_config['file_filter'].keys():
+            preprocessed_logs = []
+            process_input(preprocessed_logs, input_config, define, file_key)
+            process_output(preprocessed_logs, output_config, define, file_key)
+
+def process_input(preprocessed_logs, input_config, define, file_key, use_multithreading=False):
+    file_paths = []
+    for path in input_config['file_filter'][file_key]:
+        matched_files = glob.glob(path)
+        matched_files = [f for f in matched_files if not f.endswith('.gz')]
+        matched_files = [f for f in matched_files if not f.endswith('.tgz')]
+        if matched_files:
+            file_paths.extend(matched_files)
+            print(f"Include: {matched_files}")
+        else:
+            print(f"Warning: No files found for pattern {path}")
+
+    line_filter_def = define["line_filter_def"][input_config['line_filter']]
+
+    # 内部函数，用于处理单个文件
+    def process_single_file(log_file, line_filter_def):
+        preprocessed_data = []
+        with open(log_file, 'r', encoding='utf-8') as file:
+            for log in file:
+                preprocessed_data.append(preprocess_log(log, line_filter_def))
+        return preprocessed_data
+
+    if use_multithreading:
+        # 使用多线程处理日志文件
+        with ThreadPoolExecutor() as executor:
+            future_to_file_data = {executor.submit(process_single_file, file_path, line_filter_def): file_path for file_path in file_paths}
+            for future in as_completed(future_to_file_data):
+                file_path = future_to_file_data[future]
+                try:
+                    data = future.result()
+                    preprocessed_logs.extend(data)
+                except Exception as exc:
+                    print(f'File {file_path} generated an exception: {exc}')
+    else:
+        # 不使用多线程，直接顺序处理
+        for file_path in file_paths:
+            try:
+                data = process_single_file(file_path, line_filter_def)
+                preprocessed_logs.extend(data)
+            except Exception as exc:
+                print(f'File {file_path} generated an exception: {exc}')
+
+def process_output(preprocessed_logs, output_config, define, file_key, use_multithreading=False):
+    print(f"Processing {file_key}, total lines {len(preprocessed_logs)} ...")
+
+    def process_toplist_item(toplist_item):
+        field = toplist_item['field']
+        count = toplist_item['count']
+        merge = toplist_item['merge']
+
+        field_logs = [log[field] for log in preprocessed_logs]
+        return group_log(field_logs, merge), (file_key, count, merge, field)
+
+    def print_results(results, file_key, count, merge, field):
+        groups, group_info, group_indexes = results
+        group_counter = Counter(groups)
+        total_count = sum(group_counter.values())
+        top_groups = group_counter.most_common(count)
+
+        print(f'{file_key} Top {count} by {merge} for field {field}:')
+        for group, group_count in top_groups:
+            percentage = (group_count / total_count) * 100
+            print(f'Count: {group_count}\t| Percentage: {percentage:.2f}%\t| Content: {group_info.get(group, "N/A")}'.encode('utf-8').decode('utf-8'))
+            # 如果 output_config['print_line_num'] 有定义，则打印每个组的前几行日志
+            if 'print_line_num' in output_config:
+                print_line_num = min(output_config['print_line_num'], len(group_indexes[group]))
+                for i in group_indexes[group][:print_line_num]:
+                    if 'print_line_field' in output_config:
+                        log_str = " ".join(
+                            f"{print_field}: {preprocessed_logs[i].get(print_field, 'N/A')}"
+                            for print_field in output_config['print_line_field']
+                        )
+                        print("\t", log_str)
+                    else:
+                        print("\t", preprocessed_logs[i])
+        print('-' * 80)
+
+    if use_multithreading:
+        with ProcessPoolExecutor() as executor:
+            futures = [executor.submit(process_toplist_item, toplist_item) for toplist_item in output_config['toplist']]
+            for future in as_completed(futures):
+                results, info = future.result()
+                print_results(results, *info)
+    else:
+        for toplist_item in output_config['toplist']:
+            results, info = process_toplist_item(toplist_item)
+            print_results(results, *info)
+
+if __name__ == "__main__":
+    # 设置命令行参数解析
+    parser = argparse.ArgumentParser(description='Log Statistics Processor')
+    parser.add_argument('--conf', default='config.cluster.json', help='Path to the configuration file')
+    parser.add_argument('--mem_limit', default=10240, type=int, help='Memory limit in megabytes')
+    args = parser.parse_args()
+
+    # 设置内存限制(MB)
+    if args.mem_limit:
+        start_memory_watcher(args.mem_limit)
+
+    # 读取配置文件路径
+    config_file_path = args.conf
+
+    # 读取配置文件
+    try:
+        with open(config_file_path, 'r', encoding='utf-8') as f:
+            config = json.load(f)
+    except FileNotFoundError:
+        print(f"Error: Configuration file '{config_file_path}' not found.")
+        exit(1)
+
+    # 读取定义文件
+    try:
+        with open('define.json', 'r', encoding='utf-8') as f:
+            define = json.load(f)
+    except FileNotFoundError:
+        print("Error: Definition file 'define.json' not found.")
+        exit(1)
+
+    # 运行主处理函数
+    process_log(config['input'], config['output'], define)
---
+++ readme.md
@@ -0,0 +1,65 @@
+### 配置文件
+1. define.json 预定义的处理行为，被config.json引用
+  - line_filter_def 日志文件中字段对应的关系
+    - ONE_LINE  整行计算
+    - STANDARD  eg:[2024-04-01 20:54:53.459] [info]  [TcpConnection] [ReadLoop start, remote addr 192.168.119.100:26001] [Connection.go:238]
+    - ETCD      eg:{"level":"debug","ts":"2024-04-02T20:46:23.091+0800","caller":"v3rpc/interceptor.go:175","msg":"request stats","start time":"2024-04-02T20:46:23.091+0800","time spent":"15.958µs","remote":"192.168.119.100:53798","response 
+    type":"etcdserverpb.Cluster/MemberList","request count":-1,"request size":-1,"response count":-1,"response size":-1,"request content":""}
+    - 说明：其它的可以自行声明，声明后可以在config.json中引用
+      - seperator 定义分隔字符
+      - mapping 对应映射关系
+
+  - merge_def 日志文本的聚类方式
+
+2. config.json 定义具体的文件处理逻辑
+  - input 定义输入的文件
+  - output 定义输出的方式
+    - toplist 归类输出方式，每一项对应一个输出
+    - merge_file 是否需要合并input文件进行统计，如不合并总输出项=#toplist*#input
+    - print_line_num [可选 default:0]需要在每个分组输出后面打印的示例日志样本行数
+    - print_line_field [可选 default:全部]需要在每个分组输出后面打印的示例日志样本的field
+
+### 算法说明
+```
+K-means和DBSCAN都是聚类算法，它们用于将数据点分组成若干个簇，使得同一个簇内的点相互之间更相似，而不同簇的点相互之间差异较大。不过，这两种算法在聚类的方法和适用场景上有所不同。
+
+K-means 是一种基于划分的聚类算法，它需要预先指定簇的数量（K）。算法通过迭代过程优化簇内的点与簇中心的距离之和，最终得到相对紧凑的簇。K-means适合于簇大小相似、簇形状为球形的数据集。
+
+DBSCAN（Density-Based Spatial Clustering of Applications with Noise）是一种基于密度的聚类算法，它不需要预先指定簇的数量。DBSCAN根据核心点（在指定半径内有足够多邻居的点）和边界点（在半径内邻居数量少于指定阈值但属于某个核心点的邻域的点）的概念来形成簇。DBSCAN能够识别出任意形状的簇，并且能够将噪声点（不属于任何簇的点）识别出来。
+
+K-means输出：
+  * 一个字符串列表，其中包含了该簇的质心（中心点）的前10个最重要的术语。这些术语是通过 TfidfVectorizer 提取的特征（单词或短语），并根据它们在质心向量中的权重排序
+  * 该簇中与质心最相似的日志条目。这是通过计算簇中每个日志条目与质心的余弦相似度来确定的。在您的代码中，选择了相似度最高的日志条目作为簇的代表
+
+DBSCAN输出：
+  * 该簇的第一个代表性消息示例
+
+需要注意的是，DBSCAN的簇编号通常从0开始，而且可能包含一个特殊的簇编号（如-1），用于表示噪声点。在你的输出中，只显示了编号为0的簇，这可能是最大的簇，或者是数据中唯一显著的簇。如果有噪声点，它们可能没有在这个输出中显示(N/A)。
+```
+
+```
+K-means和DBSCAN是两种不同类型的聚类算法，它们在计算复杂度和运行时间上有所不同。在某些情况下，K-means的计算速度确实可能比DBSCAN快，但这并不是绝对的，因为它取决于多种因素，包括数据集的大小、维度、簇的数量以及算法的实现等。
+
+K-means 的计算复杂度大致是 O(nkt)，其中 n 是数据点的数量，k 是簇的数量，t 是迭代次数。K-means通常需要多次迭代才能收敛，但每次迭代通常都比较快，尤其是当k相对较小且数据集不是非常大时。K-means的一个优点是它的简单性，这使得它在低维数据上非常高效。
+
+DBSCAN 的计算复杂度在最佳情况下是 O(n log n)，但如果使用简单的邻域查询，复杂度可能会达到 O(n^2)。DBSCAN的速度取决于数据点之间距离计算的速度，以及如何有效地查询数据点的邻域。DBSCAN不需要迭代，但是它需要对每个点进行邻域查询，这在高维数据或者数据点之间的距离计算很复杂时可能会很慢。
+DBSCAN 需要存储所有数据点之间的距离矩阵，这是一个 O(N^2) 的空间复杂度，其中 N 是数据点的数量。对于大数据集，这个矩阵可能非常大。
+
+以下是一些影响两种算法速度的因素：
+  数据集大小：K-means在大数据集上可能会比DBSCAN快，因为DBSCAN的邻域查询在大数据集上可能非常慢。
+  簇的数量：K-means需要预先指定簇的数量，如果k值很大，它的速度可能会受到影响。DBSCAN不需要这样的预设，但是它的性能会受到最小点数(minPts)和邻域大小(ε)参数的影响。
+  数据维度：随着数据维度的增加，DBSCAN的性能可能会下降得更快，因为高维空间中的距离计算更加复杂。
+  数据分布：如果数据集包含大量的噪声点或者簇的形状非常不规则，DBSCAN可能会比K-means表现得更好，因为K-means假设簇是球形的。
+  算法实现：算法的具体实现也会影响其性能，例如使用高效的数据结构如KD树或球树可以加速DBSCAN的邻域查询。
+
+总的来说：大数据集用K-means，数据相似度很高用dbscan
+```
+
+### 使用方式
+python3 -m pip install -r requirements.txt
+
+python3 logStatistic.py --conf=config.cluster.json
+python3 logStatistic.py --conf=config.lua_ms.json
+python3 logStatistic.py --conf=config.go_ms.json
+python3 logStatistic.py --conf=config.etcd.json
+python3 logStatistic.py --conf=config.agent.json
